53d6fc6645b29f3cbde2167ae0cfb3c6e1b96930
DESKTOP-J3AVJEG\admin <woshidazhu689@163.com>
2021-06-09 17:25:37 +0800

    新增一些工具类

---
+++ base/src/main/java/com/bench/lang/base/object/ReflectionToStringBuilder.java
@@ -0,0 +1,103 @@
+/**
+ * BenchCode.com Inc.
+ * Copyright (c) 2005-2009 All Rights Reserved.
+ */
+package com.bench.lang.base.object;
+
+import com.bench.lang.base.array.utils.ArrayUtils;
+import com.bench.lang.base.date.utils.DateUtils;
+import com.bench.lang.base.object.utils.ObjectUtils;
+import com.bench.lang.base.sign.md5.utils.MD5Utils;
+import com.bench.lang.base.string.utils.StringMaskUtils;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+
+/**
+ * 
+ * @author chenbug
+ * 
+ * @version $Id: ReflectionToStringBuilder.java, v 0.1 2013-12-16 下午4:03:18 chenbug Exp $
+ */
+public class ReflectionToStringBuilder extends org.apache.commons.lang3.builder.ReflectionToStringBuilder {
+
+	private static final Class<?>[] IGNORE_CLASS = new Class<?>[] {};
+
+	/**
+	 * 隐藏的字段类型
+	 */
+	private String[] maskFieldNames;
+
+	/**
+	 * <p>
+	 * Constructor.
+	 * </p>
+	 * 
+	 * <p>
+	 * If the style is <code>null</code>, the default style is used.
+	 * </p>
+	 * 
+	 * @param object
+	 *            the Object to build a <code>toString</code> for, must not be <code>null</code>
+	 * @param style
+	 *            the style of the <code>toString</code> to create, may be <code>null</code>
+	 * @throws IllegalArgumentException
+	 *             if the Object passed in is <code>null</code>
+	 */
+	public ReflectionToStringBuilder(Object object, ToStringStyle style) {
+		super(object, style);
+	}
+
+	@Override
+	protected boolean accept(Field field) {
+		// TODO Auto-generated method stub
+		boolean accept = super.accept(field);
+		if (!accept) {
+			return false;
+		}
+		if (ArrayUtils.contains(IGNORE_CLASS, field.getType())) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	protected Object getValue(Field field) throws IllegalArgumentException, IllegalAccessException {
+		// TODO Auto-generated method stub
+		// 如果是字节输出，输出字节长度
+		if (field.getType().equals(byte[].class)) {
+			byte[] value = (byte[]) super.getValue(field);
+			if (value != null) {
+				return "'byte[] data,ignore output by bench,length=" + ArrayUtils.getLength(super.getValue(field)) + "，md5=" + MD5Utils.md5(value) + "'";
+			} else {
+				return null;
+			}
+		}
+		if (field.getType().equals(Date.class)) {
+			Date date = (Date) super.getValue(field);
+			if (date != null) {
+				return DateUtils.getDateExtraString(date);
+			} else {
+				return null;
+			}
+		}
+		Object value = super.getValue(field);
+		if (value == null) {
+			return value;
+		}
+		if (ArrayUtils.contains(this.maskFieldNames, field.getName())) {
+
+			return StringMaskUtils.maskAuto(ObjectUtils.toString(value));
+		}
+		return value;
+	}
+
+	public String[] getMaskFieldNames() {
+		return maskFieldNames;
+	}
+
+	public void setMaskFieldNames(String[] maskFieldNames) {
+		this.maskFieldNames = maskFieldNames;
+	}
+}
---
+++ base/src/main/java/com/bench/lang/base/string/utils/StringMaskUtils.java
@@ -0,0 +1,245 @@
+package com.bench.lang.base.string.utils;
+
+import com.bench.lang.base.BankUtils;
+import com.bench.lang.base.IDCardNoValidator;
+import com.bench.lang.base.number.utils.NumberUtils;
+
+/**
+ * 
+ * 字符串掩饰工具
+ * 
+ * @author chenbug
+ * 
+ * @version $Id: StringMaskUtils.java, v 0.1 2014-12-5 下午12:16:22 chenbug Exp $
+ */
+public class StringMaskUtils {
+
+	public static final StringMaskUtils INSTANCE = new StringMaskUtils();
+
+	/**
+	 * 自动识别
+	 * 
+	 * @param string
+	 * @return
+	 */
+	public static String maskAuto(String string) {
+		if (StringUtils.isEmpty(string)) {
+			return string;
+		}
+
+		// 可能是手机号，隐藏手机号
+		if (NumberUtils.isDigits(string) && StringUtils.length(string) == 11) {
+			return maskCell(string);
+		} // 可能是银行卡，隐藏银行卡
+		else if (NumberUtils.isDigits(string) && StringUtils.length(string) >= 15 && StringUtils.length(string) <= 25 && BankUtils.isBankCardNo(string)) {
+			return maskBankCardNo(string);
+		} // 可能是电子邮箱
+		else if (StringUtils.indexOf(string, StringUtils.AT_SIGN) > 0) {
+			return maskEmail(string);
+		}
+		// 可能是身份证号码
+		else if (IDCardNoValidator.validate(string)) {
+			return maskCertNo(string);
+		}
+		// 可能有汉字，当成真实姓名处理
+		else if (StringUtils.length(string) != StringUtils.byteLength(string)) {
+			return maskRealName(string);
+		}
+		// 默认隐藏,小于10，隐藏前3后3
+		if (string.length() < 10) {
+			return maskCenter(string, 3, StringUtils.length(string) - 4);
+		} else {
+			// 显示前3 后3分之一
+			return maskCenter(string, 3, string.length() / 3 * 2);
+		}
+	}
+
+	/**
+	 * 隐藏手机号
+	 * 
+	 * @param cell
+	 * @return
+	 */
+	public static String maskCell(String cell) {
+		return maskCenter(cell, 3, 7, StringUtils.ASTERRISK_SIGN);
+	}
+
+	/**
+	 * 隐藏邮箱
+	 * 
+	 * @param email
+	 * @return
+	 */
+	public static String maskEmail(String email) {
+		String beforeAtSign = StringUtils.substringBefore(email, StringUtils.AT_SIGN);
+		if (StringUtils.length(beforeAtSign) == 1) {
+			beforeAtSign = StringUtils.ASTERRISK_SIGN;
+		} else {
+			beforeAtSign = StringMaskUtils.maskSuffix(beforeAtSign, StringUtils.length(beforeAtSign) / 2 > 3 ? 3 : StringUtils.length(beforeAtSign) / 2);
+		}
+		return beforeAtSign + StringUtils.AT_SIGN + StringUtils.substringAfter(email, StringUtils.AT_SIGN);
+	}
+
+	/**
+	 * 隐藏银行卡号
+	 * 
+	 * @param bankCardNo
+	 * @return
+	 */
+	public static String maskBankCardNo(String bankCardNo) {
+		if (StringUtils.indexOf(bankCardNo, StringUtils.AT_SIGN) > 0) {
+			return maskEmail(bankCardNo);
+		}
+		return maskCenter(bankCardNo, 3, StringUtils.length(bankCardNo) - 4);
+	}
+
+	/**
+	 * 隐藏真实姓名
+	 * 
+	 * @param realName
+	 * @return
+	 */
+	public static String maskRealName(String realName) {
+		return StringMaskUtils.maskPrefix(realName, (int) (Math.round(StringUtils.length(realName) / 2.0d)));
+	}
+
+	/**
+	 * 隐藏证件号
+	 * 
+	 * @param certNo
+	 * @return
+	 */
+	public static String maskCertNo(String certNo) {
+		return StringMaskUtils.maskCenter(certNo, 3, StringUtils.length(certNo) - 4);
+	}
+
+	/**
+	 * 掩盖text后部，用*显示，只显示前面的showChars个字符
+	 * 
+	 * @param text
+	 * @param showChars
+	 * @return
+	 */
+	public static final String maskSuffix(String text, int showChars) {
+		return maskSuffix(text, showChars, "*");
+	}
+
+	/**
+	 * 每隔averageChars个字母,用maskChar替换Mask
+	 * 
+	 * @param text
+	 * @param averageChars
+	 * @param maskChar
+	 * @return
+	 */
+	public static String mask(String text, int averageChars, char maskChar) {
+		return mask(text, averageChars, new String(new char[] { maskChar }));
+	}
+
+	/**
+	 * 每隔averageChars个字母,用maskChar替换Mask
+	 * 
+	 * @param text
+	 * @param averageChars
+	 * @param maskChar
+	 * @return
+	 */
+	public static String mask(String text, int averageChars, String maskChar) {
+		if (StringUtils.isEmpty(text)) {
+			return StringUtils.defaultString(text);
+		}
+		if (averageChars < 0) {
+			averageChars = 0;
+		}
+		maskChar = StringUtils.trim(maskChar);
+		if (StringUtils.isEmpty(maskChar)) {
+			maskChar = "*";
+		}
+		StringBuffer buf = new StringBuffer();
+		int startIndex = 0;
+		int endIndex = averageChars;
+		while (endIndex < text.length()) {
+			buf.append(StringUtils.substring(text, startIndex, endIndex));
+			startIndex = endIndex + maskChar.length();
+			endIndex = startIndex + averageChars;
+			if (startIndex > text.length()) {
+				buf.append(StringUtils.substring(maskChar, 0, text.length() - startIndex));
+			} else {
+				buf.append(maskChar);
+			}
+		}
+		if (startIndex == 0) {
+			buf.append(text);
+		} else if (startIndex <= text.length()) {
+			buf.append(StringUtils.substring(text, startIndex, NumberUtils.max(new int[] { endIndex, text.length() })));
+		}
+		return buf.toString();
+
+	}
+
+	/**
+	 * 掩盖text后部，用maskChar显示，只显示前面的showChars个字符
+	 * 
+	 * @param text
+	 * @param showChars
+	 * @return
+	 */
+	public static final String maskSuffix(String text, int showChars, String maskChar) {
+		if (StringUtils.isEmpty(text)) {
+			return StringUtils.defaultString(text);
+		}
+		return maskSuffix(text, showChars, maskChar, text.length() - showChars);
+	}
+
+	public static final String maskSuffix(String text, int showChars, String maskChar, int maskLength) {
+		if (StringUtils.isEmpty(text) || StringUtils.length(text) <= showChars) {
+			return text;
+		}
+		return StringUtils.substring(text, 0, showChars) + StringUtils.fillSuffix(StringUtils.EMPTY_STRING, maskChar, maskLength);
+	}
+
+	public static final String maskCenter(String text, int startIndex, int endIndex) {
+		return maskCenter(text, startIndex, endIndex, StringUtils.ASTERRISK_SIGN);
+	}
+
+	public static final String maskCenter(String text, int startIndex, int endIndex, String maskChar) {
+		if (StringUtils.isEmpty(text)) {
+			return text;
+		}
+		return StringUtils.substring(text, 0, startIndex)
+				+ StringUtils.fillSuffix(StringUtils.EMPTY_STRING, maskChar, text.length() > endIndex ? (endIndex - startIndex) + 1 : text.length() - startIndex + 1)
+				+ StringUtils.substring(text, endIndex + 1);
+	}
+
+	/**
+	 * 掩盖text前部，用*显示，只显示后面的showChars个字符
+	 * 
+	 * @param text
+	 * @param showChars
+	 * @return
+	 */
+	public static final String maskPrefix(String text, int showChars) {
+		return maskPrefix(text, showChars, "*");
+	}
+
+	/**
+	 * 掩盖text前部，用maskChar显示，只显示后面的showChars个字符
+	 * 
+	 * @param text
+	 * @param showChars
+	 * @return
+	 */
+	public static final String maskPrefix(String text, int showChars, String maskChar) {
+		if (StringUtils.isEmpty(text)) {
+			return StringUtils.defaultString(text);
+		}
+		return maskPrefix(text, showChars, maskChar, text.length() - showChars);
+	}
+
+	public static final String maskPrefix(String text, int showChars, String maskChar, int maskLength) {
+		if (StringUtils.isEmpty(text) || StringUtils.length(text) <= showChars) {
+			return text;
+		}
+		return StringUtils.fillSuffix(StringUtils.EMPTY_STRING, maskChar, maskLength) + StringUtils.substring(text, text.length() - showChars, text.length());
+	}
+}
